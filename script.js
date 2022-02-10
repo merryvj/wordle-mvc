dd47f556ab864c06058d76917a74c08926c1b45f
Mary Jiang <41601131+merryvj@users.noreply.github.com>
2022-02-09 22:00:54 -0500

    clean up comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 class Model {
   constructor() {
-    this.wotd = "crane";
-
+    this.wotd = "crane"; //todo call dynamically
+    this.guessesLeft = 6;
     this.guesses = [];
   }
 
+  getGuesses() {
+      return this.guesses;
+  }
   addGuess(guess) {
       //TODO: validate guess
     let word = [];
@@ -30,6 +33,7 @@ class Model {
 
     console.log(this.guesses);
   }
+
 }
 
 class View {
@@ -85,70 +89,8 @@ class Controller {
 
   handleGuessInput = (guess) => {
     this.model.addGuess(guess);
+    this.view.displayGuesses(this.model.getGuesses());
   };
 }
 
 const app = new Controller(new Model(), new View());
-
-// const word = "graph"; //replace this with API call
-// let dictionary = [ //replace this as well!
-//     "bagel",
-//     "earth",
-//     "shelf",
-//     "field",
-//     "frame",
-//     "amaze"
-// ]
-// let guessesLeft = 6;
-
-// let guesses = [
-
-// ]
-
-// //fetching word of day
-// function getWord() {
-
-// }
-// //submitting a guess - client input, check if in dictionary
-// function submitGuess() {
-//     let guess = input.value;
-//     console.log(guess);
-
-//     if (dictionary.includes(guess)) {
-//         validateGuess(guess);
-//         guessesLeft--;
-//         instructions.innerHTML = `You have ${guessesLeft} guesses left`;
-//         input.value = '';
-//     } else {
-//         alert("Please enter a valid 5-letter word.");
-//     }
-// }
-
-// //validating guess compared to word of day
-// function validateGuess(guess) {
-
-//     let guessLetters = [];
-
-//     for (let i = 0; i < guess.length; i++) {
-//         let state = "grey";
-//         let letter = guess.charAt(i);
-//         if (word.includes(letter)) {
-//             state = "yellow";
-
-//             if (word.charAt(i) === letter) {
-//                 state = "green";
-//             }
-//         }
-
-//         guessLetters.push({
-//             "index": i,
-//             "letter": letter,
-//             "state": state
-//         });
-//     }
-
-//     guesses.push(guessLetters);
-//     showGuessResults();
-// }
-
-//handing game states
